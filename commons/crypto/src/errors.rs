87f209e498f533faf6532da7ae702223996396c7
mineshp <mineshp@mechasystems.com>
2023-10-06 11:07:31 +0530

    dir creation issue fixed

--- commons/crypto/src/errors.rs
+++ commons/crypto/src/errors.rs
@@ -1,5 +1,5 @@
-use std::fmt;
 use sentry_anyhow::capture_anyhow;
+use std::fmt;
 use tracing::error;
 use tracing_opentelemetry_instrumentation_sdk::find_current_trace_id;
 
@@ -13,6 +13,7 @@ pub enum CryptoErrorCodes {
     UpdateSignerError,
     PemDeserializeError,
     ExtractSubjectNameError,
+    FilePathError,
     #[default]
     UnknownError,
 }
@@ -21,14 +22,25 @@ impl fmt::Display for CryptoErrorCodes {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             CryptoErrorCodes::GenerateCSRError => write!(f, "CryptoErrorCodes: GenerateCSRError"),
-            CryptoErrorCodes::GeneratePrivateKeyError => write!(f, "CryptoErrorCodes: GeneratePrivateKeyError"),
-            CryptoErrorCodes::OpenPrivateKeyError => write!(f, "CryptoErrorCodes: OpenPrivateKeyError"),
-            CryptoErrorCodes::ReadPrivateKeyError => write!(f, "CryptoErrorCodes: ReadPrivateKeyError"),
+            CryptoErrorCodes::GeneratePrivateKeyError => {
+                write!(f, "CryptoErrorCodes: GeneratePrivateKeyError")
+            }
+            CryptoErrorCodes::OpenPrivateKeyError => {
+                write!(f, "CryptoErrorCodes: OpenPrivateKeyError")
+            }
+            CryptoErrorCodes::ReadPrivateKeyError => {
+                write!(f, "CryptoErrorCodes: ReadPrivateKeyError")
+            }
             CryptoErrorCodes::LoadSignerError => write!(f, "CryptoErrorCodes: LoadSignerError"),
             CryptoErrorCodes::UpdateSignerError => write!(f, "CryptoErrorCodes: UpdateSignerError"),
-            CryptoErrorCodes::PemDeserializeError => write!(f, "CryptoErrorCodes: PemDeserializeError"),
+            CryptoErrorCodes::PemDeserializeError => {
+                write!(f, "CryptoErrorCodes: PemDeserializeError")
+            }
             CryptoErrorCodes::UnknownError => write!(f, "CryptoErrorCodes: UnknownError"),
-            CryptoErrorCodes::ExtractSubjectNameError => write!(f, "CryptoErrorCodes: ExtractSubjectNameError"),
+            CryptoErrorCodes::ExtractSubjectNameError => {
+                write!(f, "CryptoErrorCodes: ExtractSubjectNameError")
+            }
+            CryptoErrorCodes::FilePathError => write!(f, "CryptoErrorCodes: FilePathError"),
         }
     }
 }
@@ -41,7 +53,11 @@ pub struct CryptoError {
 
 impl std::fmt::Display for CryptoError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "CryptoErrorCodes:(code: {:?}, message: {})", self.code, self.message)
+        write!(
+            f,
+            "CryptoErrorCodes:(code: {:?}, message: {})",
+            self.code, self.message
+        )
     }
 }
 
