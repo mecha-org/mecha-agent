bb9c5b8bf023feae82124c014482a8b67aa38686
mineshp <mineshp@mechasystems.com>
2024-04-03 14:54:58 +0530

    deprovision fixes

--- commons/kv_store/src/lib.rs
+++ commons/kv_store/src/lib.rs
@@ -14,9 +14,9 @@ const PACKAGE_NAME: &str = env!("CARGO_PKG_NAME");
 static DATABASE_STORE_FIILE_PATH: &str = "~/.mecha/agent/db";
 // Singleton database connection
 lazy_static! {
-    static ref DATABASE: Arc<Mutex<Db>> = {
-        let file_path = fs::construct_dir_path(DATABASE_STORE_FIILE_PATH.clone()).unwrap();
-        Arc::new(Mutex::new(sled::open(&file_path).unwrap()))
+    static ref DATABASE: Option<Arc<Mutex<Db>>> = {
+        let file_path = fs::construct_dir_path(DATABASE_STORE_FIILE_PATH).unwrap();
+        Some(Arc::new(Mutex::new(sled::open(&file_path).unwrap())))
     };
 }
 
@@ -34,48 +34,51 @@ impl KeyValueStoreClient {
             "init - settings: {:?}",
             settings
         );
-        let db = match DATABASE.lock() {
-            Ok(d) => d,
-            Err(e) => {
-                error!(
-                    func = "set",
-                    package = PACKAGE_NAME,
-                    "failed to acquire lock on db - {}",
-                    e
-                );
-                bail!(KeyValueStoreError::new(
-                    KeyValueStoreErrorCodes::DbAcquireLockError,
-                    format!("error acquiring lock on set - {}", e),
-                    true
-                ))
-            }
-        };
-
-        for (key, value) in settings {
-            debug!(
-                func = "set",
-                package = PACKAGE_NAME,
-                "inserting key: {}, value: {}",
-                key,
-                value
-            );
-            match db.insert(key, value.as_str()) {
-                Ok(_) => {}
+        if let Some(database) = DATABASE.as_ref() {
+            let db = match database.lock() {
+                Ok(d) => d,
                 Err(e) => {
                     error!(
                         func = "set",
                         package = PACKAGE_NAME,
-                        "failed to insert value into db - {}",
+                        "failed to acquire lock on db - {}",
                         e
                     );
                     bail!(KeyValueStoreError::new(
-                        KeyValueStoreErrorCodes::InsertError,
-                        format!("error inserting value into db - {}", e),
+                        KeyValueStoreErrorCodes::DbAcquireLockError,
+                        format!("error acquiring lock on set - {}", e),
                         true
                     ))
                 }
             };
+
+            for (key, value) in settings {
+                debug!(
+                    func = "set",
+                    package = PACKAGE_NAME,
+                    "inserting key: {}, value: {}",
+                    key,
+                    value
+                );
+                match db.clone().insert(key, value.as_str()) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        error!(
+                            func = "set",
+                            package = PACKAGE_NAME,
+                            "failed to insert value into db - {}",
+                            e
+                        );
+                        bail!(KeyValueStoreError::new(
+                            KeyValueStoreErrorCodes::InsertError,
+                            format!("error inserting value into db - {}", e),
+                            true
+                        ))
+                    }
+                };
+            }
         }
+
         info!(
             func = "set",
             package = PACKAGE_NAME,
@@ -85,41 +88,95 @@ impl KeyValueStoreClient {
     }
     pub fn get(&self, key: &str) -> Result<Option<String>> {
         trace!(func = "get", package = PACKAGE_NAME, " key: {}", key);
-        let db = match DATABASE.lock() {
-            Ok(d) => d,
-            Err(e) => {
-                error!(
-                    func = "get",
-                    package = PACKAGE_NAME,
-                    "failed to acquire lock on db - {}",
-                    e
-                );
-                bail!(KeyValueStoreError::new(
-                    KeyValueStoreErrorCodes::DbAcquireLockError,
-                    format!("error acquiring lock on get - {}", e),
+        if let Some(database) = DATABASE.as_ref() {
+            let db = match database.lock() {
+                Ok(d) => d,
+                Err(e) => {
+                    error!(
+                        func = "set",
+                        package = PACKAGE_NAME,
+                        "failed to acquire lock on db - {}",
+                        e
+                    );
+                    bail!(KeyValueStoreError::new(
+                        KeyValueStoreErrorCodes::DbAcquireLockError,
+                        format!("error acquiring lock on set - {}", e),
+                        true
+                    ))
+                }
+            };
+            let last_inserted = db.get(key);
+            match last_inserted {
+                Ok(s) => {
+                    debug!(
+                        func = "get",
+                        package = PACKAGE_NAME,
+                        "retrieved value from db key - {}",
+                        key
+                    );
+                    return Ok(s.map(|s| String::from_utf8(s.to_vec()).unwrap()));
+                }
+                Err(e) => bail!(KeyValueStoreError::new(
+                    KeyValueStoreErrorCodes::RetrieveValueError,
+                    format!(
+                        "Error retrieving value from db key - {}, error - {}",
+                        key, e
+                    ),
                     true
-                ))
+                )),
             }
-        };
-        let last_inserted = db.get(key);
-        match last_inserted {
-            Ok(s) => {
-                debug!(
-                    func = "get",
-                    package = PACKAGE_NAME,
-                    "retrieved value from db key - {}",
-                    key
-                );
-                Ok(s.map(|s| String::from_utf8(s.to_vec()).unwrap()))
+        }
+        Ok(None)
+    }
+    pub fn flush_database(&self) -> Result<bool> {
+        trace!(func = "reset_connection", package = PACKAGE_NAME, "init");
+        if let Some(database) = DATABASE.as_ref() {
+            let db = match database.lock() {
+                Ok(d) => d,
+                Err(e) => {
+                    error!(
+                        func = "reset_connection",
+                        package = PACKAGE_NAME,
+                        "failed to acquire lock on db - {}",
+                        e
+                    );
+                    bail!(KeyValueStoreError::new(
+                        KeyValueStoreErrorCodes::DbAcquireLockError,
+                        format!("error acquiring lock on set - {}", e),
+                        true
+                    ))
+                }
+            };
+            match db.flush() {
+                Ok(_) => {
+                    info!(
+                        func = "reset_connection",
+                        package = PACKAGE_NAME,
+                        "database connection reset!"
+                    );
+                    db.clear();
+                    Ok(true)
+                }
+                Err(e) => {
+                    error!(
+                        func = "reset_connection",
+                        package = PACKAGE_NAME,
+                        "failed to reset database connection - {}",
+                        e
+                    );
+                    bail!(KeyValueStoreError::new(
+                        KeyValueStoreErrorCodes::UnknownError,
+                        format!("error resetting database connection - {}", e),
+                        true
+                    ))
+                }
             }
-            Err(e) => bail!(KeyValueStoreError::new(
-                KeyValueStoreErrorCodes::RetrieveValueError,
-                format!(
-                    "Error retrieving value from db key - {}, error - {}",
-                    key, e
-                ),
+        } else {
+            bail!(KeyValueStoreError::new(
+                KeyValueStoreErrorCodes::DbNotInitialized,
+                "database not initialized".to_string(),
                 true
-            )),
+            ))
         }
     }
 }
